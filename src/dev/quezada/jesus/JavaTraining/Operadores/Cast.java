52bda936eb55a8a1dffb875b5790bdb7cd6e856c
Jesus <jesus.quezada.dev@gmail.com>
2016-08-09 11:14:16 -0500

    Cast

---
+++ src/dev/quezada/jesus/JavaTraining/Operadores/Cast.java
@@ -0,0 +1,18 @@
+package dev.quezada.jesus.JavaTraining.Operadores;
+
+public class Cast {
+
+	public static void main(String[] args) {
+
+		//Cast
+		int variableX = 50, variableY = 10;
+		float variableZ = 5.5f;
+		int resultado;
+		
+		resultado = variableX + (int)variableZ; //55.5
+		System.out.println(resultado);
+		
+
+	}
+
+}
