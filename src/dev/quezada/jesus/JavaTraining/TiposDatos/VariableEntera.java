40fb9ce6dd665dfe0602aa781ac16ad051924d64
Jesus <jesus.quezada.dev@gmail.com>
2016-08-09 01:27:24 -0500

    Variable entera

---
+++ src/dev/quezada/jesus/JavaTraining/TiposDatos/VariableEntera.java
@@ -0,0 +1,18 @@
+package dev.quezada.jesus.JavaTraining.TiposDatos;
+
+public class VariableEntera {
+
+	public static void main(String[] args) {
+
+		// Variable entera "int"
+				int version;
+				version = 9;
+				version = 10;
+				System.out.println(version);
+				
+				int versionJava = 10;
+				System.out.println(versionJava);
+
+	}
+
+}
