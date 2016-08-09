5b1482f9b2b83841ff16f23188942e4f66db6989
Jesus <jesus.quezada.dev@gmail.com>
2016-08-09 16:40:11 -0500

    Métodos de formato Parte 1

---
+++ src/dev/quezada/jesus/JavaTraining/Strings/MetodosFormato1.java
@@ -0,0 +1,18 @@
+package dev.quezada.jesus.JavaTraining.Strings;
+
+public class MetodosFormato1 {
+
+	public static void main(String[] args) {
+		// Metodos de formato Parte 1
+		
+		String mensaje = "    Hola, soy un String    ";
+		
+		System.out.println(mensaje.toUpperCase());
+		System.out.println(mensaje.toLowerCase());
+
+		System.out.println(mensaje.trim());
+		System.out.println(mensaje);
+
+	}
+
+}
