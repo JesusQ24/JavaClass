ea212bf2d703f2ef723c945a00b78278365ebb14
Jesus <jesus.quezada.dev@gmail.com>
2016-08-09 11:29:21 -0500

    Relacionales

---
+++ src/dev/quezada/jesus/JavaTraining/Operadores/Relacionales.java
@@ -0,0 +1,31 @@
+package dev.quezada.jesus.JavaTraining.Operadores;
+
+public class Relacionales {
+
+	public static void main(String[] args) {
+		// Operadores relacionales
+
+		int variable1 = 300, variable2 = 200;
+		boolean resultado;
+		
+		resultado = variable1 > variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 < variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 >= variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 <= variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 == variable2;
+		System.out.println(resultado);
+		
+		resultado = variable1 != variable2;
+		System.out.println(resultado);
+		
+	}
+
+}
