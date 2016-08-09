b4fabce40b3c2705776e51d6833e8489aadc3e18
Jesus <jesus.quezada.dev@gmail.com>
2016-08-09 15:30:13 -0500

    Cadenas en Java

---
+++ src/dev/quezada/jesus/JavaTraining/Strings/Cadenas.java
@@ -0,0 +1,17 @@
+package dev.quezada.jesus.JavaTraining.Strings;
+
+public class Cadenas {
+
+	public static void main(String[] args) {
+		// String = Clase
+		
+		String mensaje = "Hola, soy un string";
+		
+		String mensaje2 = new String("Texto");
+		
+		System.out.println(mensaje);
+		System.out.println(mensaje2);
+
+	}
+
+}
