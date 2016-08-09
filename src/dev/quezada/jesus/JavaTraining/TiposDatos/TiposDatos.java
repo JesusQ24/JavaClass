b8ac0bac2a58cb482db7cdeeacf9fc8eaacfe4c3
Jesus <jesus.quezada.dev@gmail.com>
2016-08-09 01:31:02 -0500

    Tipos de datos

---
+++ src/dev/quezada/jesus/JavaTraining/TiposDatos/TiposDatos.java
@@ -0,0 +1,23 @@
+package dev.quezada.jesus.JavaTraining.TiposDatos;
+
+public class TiposDatos {
+
+	public static void main(String[] args) {
+		
+		int versionJava = 9;
+		
+		float versionEspecifica = 9.8f;
+		
+		char letra = 'a';
+				
+		char numero = 97;
+		
+		System.out.println(letra);
+		System.out.println(numero);
+		
+		boolean valor1 = true;
+		boolean valor2 = false;
+		
+	}
+
+}
