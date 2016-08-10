ce13c44571f439e0f2412e59b5b7aa5ac4d86da7
Jesus <jesus.quezada.dev@gmail.com>
2016-08-09 21:14:34 -0500

    Concatenar

---
+++ src/dev/quezada/jesus/JavaTraining/Strings/Concatenar.java
@@ -0,0 +1,16 @@
+package dev.quezada.jesus.JavaTraining.Strings;
+
+public class Concatenar {
+
+	public static void main(String[] args) {
+		// Concatenar
+		
+	String nombre = "Codi";
+	String curso = "Curso de Java";
+	
+	String mensaje = "Hola, bienvenido " + nombre + " al " + curso + "!";
+	System.out.println(mensaje);
+
+	}
+
+}
