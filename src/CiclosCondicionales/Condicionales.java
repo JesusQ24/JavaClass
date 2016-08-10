727ab20e794d76bc95a8c59fd80adcd59052802e
Jesus <jesus.quezada.dev@gmail.com>
2016-08-09 21:41:11 -0500

    Condicionales

---
+++ src/CiclosCondicionales/Condicionales.java
@@ -0,0 +1,24 @@
+package CiclosCondicionales;
+
+public class Condicionales {
+
+	public static void main(String[] args) {
+	// Condicionales
+	//if	
+		
+	String colorluz = "Rojo";
+	
+	if (colorluz.equals("Verde")) {
+		
+		System.out.println("Puede continuar");
+		
+	}else {
+		
+		System.out.println("Alto total");
+		
+	}
+	
+		
+	}
+
+}
