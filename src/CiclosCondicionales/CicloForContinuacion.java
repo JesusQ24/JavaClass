46dabd42732431349d6348d293b57b4d5a3e64d9
Jesus <jesus.quezada.dev@gmail.com>
2016-08-12 21:47:46 -0500

    Ciclo For Continuación

---
+++ src/CiclosCondicionales/CicloForContinuacion.java
@@ -0,0 +1,17 @@
+package CiclosCondicionales;
+
+public class CicloForContinuacion {
+
+	public static void main(String[] args) {
+		// Ciclo For Continuación
+		
+		for (int numero = 1; numero <101; numero++) {
+			if( numero %2 == 0 ) {
+				System.out.println(numero);
+			}
+			
+		}
+
+	}
+
+}
